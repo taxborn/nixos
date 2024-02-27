cb3193933f4d646d8a04deb386adb9135442e8b5
Braxton Fair <hello@taxborn.com>
2024-02-27 09:57:46 -0600

    nixos starting point

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+	turing = nixpkgs.lib.nixosSystem {
+	  specialArgs = {inherit inputs;};
+	  modules = [ 
+	    ./hosts/turing/configuration.nix
+	    inputs.home-manager.nixosModules.default
+	  ];
+	};
+
+	euclid = nixpkgs.lib.nixosSystem {
+	  specialArgs = {inherit inputs;};
+	  modules = [ 
+	    ./hosts/euclid/configuration.nix
+	    inputs.home-manager.nixosModules.default
+	  ];
+	};
+      };
+    };
+}
