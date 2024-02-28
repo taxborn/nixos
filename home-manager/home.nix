2fa90489078041b9e8fe20baccecae42958120e3
Braxton Fair <hello@taxborn.com>
2024-02-27 23:30:06 -0600

    simplify home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,39 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "taxborn";
   home.homeDirectory = "/home/taxborn";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "23.11"; # Don't change
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     ".config/nvim" = {
       source = ../dotfiles/nvim;
@@ -47,31 +25,28 @@
     };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/taxborn/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
 
-  programs.git = {
-    enable = true;
-    userName = "Braxton Fair";
-    userEmail = "hello@taxborn.com";
-  };
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+      userName = "Braxton Fair";
+      userEmail = "hello@taxborn.com";
+
+      signing.key = "CA973F0FD0F2AF3A";
+      signing.signByDefault = true;
+    };
+
+    # So HM can manage bash and include the session variables
+    bash.enable = true;
+
+    lazygit.enable = true;
+
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+  };
 }
