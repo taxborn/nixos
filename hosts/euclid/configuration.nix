7ec0941527a0b8300ea639e4c1bbcb0ad21bdcd8
Braxton Fair <hello@taxborn.com>
2024-02-27 23:55:12 -0600

    make nixos more laptop-friendly

--- hosts/euclid/configuration.nix
+++ hosts/euclid/configuration.nix
@@ -18,11 +18,28 @@
     alias rebuild="sudo nixos-rebuild switch --flake /home/taxborn/dev/code/nixos/#euclid"
   '';
 
-  services.xserver.dpi = 261; # since I got a HiDPI display
-  # TODO: why is the mouse so god damn tiny
+  services.xserver = {
+    # TODO: why is the mouse so god damn tiny
+    dpi = 180; # 261 is the 'actual' dpi, but I find this value to be better
+
+    libinput = {
+      touchpad.tapping = false;
+      touchpad.disableWhileTyping = true;
+      touchpad.scrollMethod = "twofinger";
+      touchpad.naturalScrolling = false;
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     brightnessctl
   ];
+
+  powerManagement.enable = true;
+  powerManagement.cpuFreqGovernor = null; # will be managed by tlp
+
+  services.dbus.enable = true;
+  services.acpid.enable = true;
+  services.upower.enable = true;
+  services.tlp.enable = true;
 }
 
