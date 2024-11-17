45e4aa2527032e7ee562959904f88020f594a9b8
Braxton Fair <hello@taxborn.com>
2024-11-17 02:54:18 -0600

    feat: allow uranium to ssh everywhere

--- hosts/common/users/taxborn.nix
+++ hosts/common/users/taxborn.nix
@@ -11,9 +11,10 @@
     extraGroups = [ "wheel" "networkmanager" "audio" "video" ];
 
     initialHashedPassword = "$y$j9T$sfTsI6QwCtxiJbDQP2XnY/$L.bFrSjEG/Hj3lTz7JZv6by/iYH.b87FaV.FIwfj1K6";
-    # for uranium, we only want tungsten to be allowed in.
+    # we want tungsten and uranium to be able to ssh into devices
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJvlhtxQLO5zhlTcztBMOpkL+xSGWVWd/dTEu+Jb4zsM taxborn@tungsten (hello@taxborn.com)"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDex5NmMiZqlhAaXQb7XFS8LlwVtWm7r6lMVgoD2Gn66 taxborn@uranium (hello@taxborn.com)"
     ];
   };
 
