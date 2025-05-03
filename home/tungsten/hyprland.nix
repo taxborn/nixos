d7bcd844083595e4aaebaa570ca8c7a7b9c3ffd4
Braxton Fair <hello@taxborn.com>
2025-05-03 11:50:56 -0500

    feat: tungsten display

--- home/tungsten/hyprland.nix
+++ home/tungsten/hyprland.nix
@@ -1,5 +1,11 @@
-{ ... }: {
+{ pkgs, ... }: {
   wayland.windowManager.hyprland.settings = {
-    monitor = [ "eDP-1,3456x2560@60,0x0,1" ];
+    monitor = [ "eDP-1,3456x2160@60,auto,1.6" ];
+  };
+
+  home.pointerCursor = {
+    name = "Adwaita";
+    package = pkgs.adwaita-icon-theme;
+    size = 48; # Bigger cursor for 4K
   };
 }
